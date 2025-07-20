74667f160b07d83d03c6b5ca2c1b1acfbb6ede90
Vicmuratha <136014248+Vicmuratha@users.noreply.github.com>
2025-07-20 18:09:01 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 header('Content-Type: application/json');
 
-// Invalidate session
+
 session_destroy();
 
 echo json_encode(['success' => true, 'message' => 'Logged out successfully']);
